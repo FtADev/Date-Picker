d572c6cf46dd590ec4606e1611110d96dc0915a3
fatemeh <akhlaghi.fatemeh@gmail.com>
2023-10-01 16:09:55 +0330

    Add customization on main

--- lib/all_providers.dart
+++ lib/all_providers.dart
@@ -1,7 +1,10 @@
 import 'package:custom_date_picker/provider/date_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'provider/main_provider.dart';
+
 
 class AllProvider {
   static final dateProvider = ChangeNotifierProvider.autoDispose((_) => DateProvider());
+  static final mainProvider = ChangeNotifierProvider.autoDispose((_) => MainProvider());
 }
